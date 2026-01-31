4cdc7c5565e9f9c32dcc24ec0b0d5728673cdb2a
Gaven Pearl <127455079+xGOBx@users.noreply.github.com>
2026-01-31 16:42:30 -0500

    Changed port from 5000 to 5012

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Audio.Controllers;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.UseUrls("http://localhost:5012");
 
 // Add services
 builder.Services.AddSingleton<AudioCaptureService>();
